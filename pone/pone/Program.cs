36ddf26b010d40bfb7d963d13bb94cfc3d2b1997
SanjanaS94 <sanjana9422@gmail.com>
2023-04-23 21:12:34 +0100

    simple addition

---
+++ pone/pone/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using pone.src;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter A and B to get C");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            arithmetic add = new arithmetic();
+            int c = add.addition(a, b);
+
+            Console.WriteLine("The C vaule is : " + c);
+        }
+    }
+}
---
+++ pone/pone/src/arithmetic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace pone.src
+{
+	public class arithmetic
+	{
+
+		public int addition(int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
+
